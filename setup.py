7e9ba303f30879b995b538d094fdc8fef453046d
eoin <awardnopoints@gmail.com>
2018-03-13 12:42:22 +0000

    reformatted for pip install functionality

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='bikefind',
       version='0.1',
       description='finds bikes in the city centre',
       author='Eoin Moore, Martin Casey, Conor Hopkins',
-      packages=['bikefind','bikefind.templates'],
+      packages=['bikefind'],
       entry_points={
               'console_scripts':['bikefind=bikefind.app:app.run']
               }
