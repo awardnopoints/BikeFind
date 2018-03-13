9cd979c595260679f22662ed412d5797bf815e83
eoin <awardnopoints@gmail.com>
2018-03-13 16:09:16 +0000

    template for mysql-to-flask-to-template

--- bikefind/app.py
+++ bikefind/app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL
+
 app = Flask(__name__)
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'mytest'
+mysql = MySQL(app)
 
 @app.route("/")
+
 def main():
-    return render_template('testindex.html')
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT * from new_table''')
+    rv = cur.fetchall()
+    return str(rv)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
