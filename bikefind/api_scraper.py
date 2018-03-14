96d6992178ee776f69e56f6dd3e9cf153a468479
Martin Casey <caseym3@tcd.ie>
2018-03-14 00:08:53 +0000

    sqlalchemy basic connection established with local test db. requirements file started

--- bikefind/api_scraper.py
+++ bikefind/api_scraper.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
 import requests
+from sqlalchemy import create_engine
 
+db_connection_string ='mysql+cymysql://root:password@localhost:3306/test_db'
+engine = create_engine(db_connection_string)
+connection = engine.connect()
 
+print('Table names for test_db: ', engine.table_names(), '\n')
 
-r = requests.get('https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=6e19678db44aa0bfdb4632faba1f58723758a2c4')
+bikes_connection_string ='https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=6e19678db44aa0bfdb4632faba1f58723758a2c4'
+r = requests.get(bikes_connection_string)
 for obj in r.json()[:20]:
     print(obj)
     print('\n')
-    
-r2 = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Dublin&appid=416123cec041d7c358e497cd73c9657e').json()
+  
+weather_connection_string = 'http://api.openweathermap.org/data/2.5/weather?q=Dublin&appid=416123cec041d7c358e497cd73c9657e'  
+r2 = requests.get(weather_connection_string).json()
 print(r2)
