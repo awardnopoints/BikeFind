630dda6b61591d7e21fe1052d871f226e40496c6
Conor Hopkins <conor.hopkins@ucdconnect.ie>
2018-03-15 17:37:26 +0000

    Fixed dynamicData and weatherData. Current version working

--- bikefind/webscraper.py
+++ bikefind/webscraper.py
@@ -1,13 +1,12 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm.session import sessionmaker
-# import model from db test class
 from bikefind.test_dbclass import staticData, dynamicData, weatherData
 import requests
 import time
 
 # connect to local db 'test_db'
-#This line needs to be changed after pulling
-db_connection_string ='mysql+cymysql://root:password@localhost:3306/test'
+#This line is just a dummy and needs to be changed after pulling from remote
+db_connection_string ='mysql+cymysql://root:goop9oxt@localhost:3306/test'
 engine = create_engine(db_connection_string)
 
 Session = sessionmaker(bind=engine)
@@ -15,28 +14,23 @@ session = Session()
 
 # get from jcdecaux api and store data in list of json objects (station_info_list)
 bikes_connection_string ='https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=6e19678db44aa0bfdb4632faba1f58723758a2c4'
-#r = requests.get(bikes_connection_string)
-#station_info_list = r.json()
 
 # get from openweathermap api and store data in dictionary
 weather_connection_string = 'http://api.openweathermap.org/data/2.5/weather?q=Dublin&appid=416123cec041d7c358e497cd73c9657e'
-#r2 = requests.get(weather_connection_string).json()
-#print(r2["weather"][0]["main"])
 
 def main():
     # add static data (once-off)
     getStaticData()
-    dynamic_id = 0
     # add dynamic data to db every 10 mins
     while(True):
 
         Session = sessionmaker(bind=engine)
         session = Session()
 
-        getDynamicData(dynamic_id)
-        getWeatherData(dynamic_id)
+        getDynamicData()
+        #Weather data needs to put on a seperate timer somehow, currently duplicate rows are being appended (w/ unique index)
+        getWeatherData()
 
-        dynamic_id+=1
         session.close()
         #600 seconds/ten minute approx (wait between end of code executing and starting again)
         print("sleeping now")
@@ -54,34 +48,26 @@ def getStaticData():
         #add to db
         add_static(address, latitude, longitude, banking)
 
-def getDynamicData(dynamic_id):
+def getDynamicData():
         r = requests.get(bikes_connection_string)
         station_info_list = r.json()
         for station in station_info_list:
+
             curr_time = station['last_update']
             # var called curr_time to avoid clash with time function used below
-            current_id = dynamic_id
             address = station['address']
             totalBikeStands = station['bike_stands']
             availableBikeStands = station['available_bike_stands']
             availableBikes = station['available_bikes']
             status = station['status']
 
-#            dynamic
-#            print(station['last_update'])
-#            print(station['address'])
-#            print(station['bike_stands'])
-#            print(station['available_bike_stands'])
-#            print(station['available_bikes'])
-#            print(station['status'])
-
+            #id is an sql keyword and is the only arg that hasn't been defined, it generates an index
             add_dynamic(id, curr_time, address, totalBikeStands, availableBikeStands, availableBikes, status)
 
-def getWeatherData(dynamic_id):
+def getWeatherData():
         r2 = requests.get(weather_connection_string)
         w_list = r2.json()
 
-        w_id = dynamic_id
         w_time = w_list['dt']
         w_mainDescription = w_list['weather'][0]['main']
         w_detailedDescription = w_list['weather'][0]['description']
@@ -98,7 +84,8 @@ def getWeatherData(dynamic_id):
         w_cloudDensity = w_list['clouds']['all']
         w_visibility = w_list['visibility']
 
-        add_weather(w_id, w_time, w_mainDescription, w_detailedDescription, w_icon, w_temp, w_maxTemp, w_minTemp, w_pressure, w_humidity, w_windSpeed, w_windAngle, w_cloudDensity, w_visibility)
+        #id is an sql keyword and is the only arg that hasn't been defined, it generates an index
+        add_weather(id, w_time, w_mainDescription, w_detailedDescription, w_icon, w_temp, w_maxTemp, w_minTemp, w_pressure, w_humidity, w_windSpeed, w_windAngle, w_cloudDensity, w_visibility)
 
 def add_static(address, latitude, longitude, banking):
     # add the code to add each row
@@ -115,17 +102,6 @@ def add_weather(w_id, w_time, w_mainDescription, w_detailedDescription, w_icon, 
     weather_row = weatherData(index = w_id, time = w_time, mainDescription = w_mainDescription, detailedDescription = w_detailedDescription, icon = w_icon, currentTemp = w_temp, maxTemp = w_maxTemp, minTemp = w_minTemp, pressure = w_pressure, humidity = w_humidity, windSpeed = w_windSpeed, windAngle = w_windAngle, cloudDensity = w_cloudDensity, visibility = w_visibility)
     session.add(weather_row)
     session.commit()
-
-##############
-# example - adding a row
-
-#dame_street = staticData(address = 'dame street',
- #                              banking = "Yes")
-
-#session.add(dame_street)
-#session.commit()
-###########
-
 
 if __name__ == '__main__':
     main()
