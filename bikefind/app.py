e075904cc4b10fb03fbdb64b988b4427d47402a3
Martin Casey <caseym3@tcd.ie>
2018-03-21 19:30:00 +0000

    flask logging issue fixed

--- bikefind/app.py
+++ bikefind/app.py
@@ -1,23 +1,24 @@
 from flask import Flask, render_template, jsonify
 import pandas as pd
-import bikefind.webscraper
-from bikefind import webscraper
-
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
+db_connection_string = "mysql+cymysql://conor:team0db1@team0db.cojxdhcdsq2b.us-west-2.rds.amazonaws.com/test2"
+#db_connection_string ='mysql+cymysql://root:password@localhost:3306/test_db'
+engine = create_engine(db_connection_string)
+
 @app.route('/')
 def index():
-
     return render_template('index.html')
 
 @app.route('/staticTest')
 def getStaticTest():
-    staticDataTable = pd.read_sql_table('staticData', webscraper.engine)
+    staticDataTable = pd.read_sql_table('staticData', engine)
     #staticDataDictArray = staticDataTable.T.to_dict().values()
     staticDataDict = staticDataTable.to_dict(orient='index')
     staticData = jsonify(staticDataDict)
     return staticData
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
