a6c6885c9e487c931e890771b8a5440ebdc63047
conor <conor.hopkins@ucdconnect.ie>
2018-03-25 12:49:31 +0000

    bf_display now used a wrapped entry point

--- bikefind/app.py
+++ bikefind/app.py
@@ -20,5 +20,8 @@ def getStaticTest():
     staticData = jsonify(staticDataDict)
     return staticData
 
+def appWrapper():
+    app.run(host='0.0.0.0', port=5001)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
