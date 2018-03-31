c65ddbed10ce91b58f3a0c40e03a8bd7ace2293c
Martin Casey <caseym3@tcd.ie>
2018-03-31 14:40:19 +0100

    added basic flask app test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,4 +1,5 @@
 import sys
+from bikefind.app import app
 sys.path.append(".")
 
 def foo(x):
@@ -10,3 +11,8 @@ def test_example():
 
 def test_complex_example():
     assert foo(55)
+
+def test_flaskapp():
+    test_client = app.test_client()
+    response = test_client.get('/', content_type = 'html/text')
+    assert response.status_code == 200
