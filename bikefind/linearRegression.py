847b4718026557bdfaa11ec585d42a77d99bbbe6
eoin <awardnopoints@gmail.com>
2018-04-04 23:30:51 +0100

    added pickle

--- bikefind/linearRegression.py
+++ bikefind/linearRegression.py
@@ -8,14 +8,21 @@ Created on Mon Apr  2 13:45:32 2018
 
 import pandas as pd
 import statsmodels.formula.api as sm
-#import datetime
+import pickle
+import time
 
 
-def main():
+def saveModel(model_name):
+    start = time.time()
     db_connection_string = "mysql+cymysql://conor:team0db1@team0db.cojxdhcdsq2b.us-west-2.rds.amazonaws.com/team0"
     
     # Add new columns to dynamicData dataframe for date, day, hour
     df_dynamic = pd.read_sql_table(table_name="dynamicData", con=db_connection_string)
+    
+    now = time.time()
+    current = now - start
+    print("Get Data:", current)
+    
     df_dynamic['datetime'] = pd.to_datetime(df_dynamic['time'] * 1000000, errors='ignore')
     df_dynamic['date'] = df_dynamic['datetime'].dt.date
     df_dynamic['day'] = df_dynamic['datetime'].dt.weekday_name
@@ -28,13 +35,55 @@ def main():
     df_weather['day'] = df_weather['datetime'].dt.weekday_name
     df_weather['hour'] = df_weather['datetime'].dt.hour
     
+    now = time.time()
+    current = now - start
+    print("Build weather dataframe:", current)
+
     # Merge dynamic and weather on date, day, hour
     df_merge = pd.merge(df_dynamic, df_weather, on=['date', 'day', 'hour'])
     
     # Build linear regression to predict availableBikes
-    myformula = "availableBikes ~ C(day) + C(status) + C(hour) + C(address) + C(detailedDescription) + currentTemp + pressure + humidity + windSpeed + windAngle + cloudDensity + visibility"
+    myformula = "availableBikes ~ C(day) + C(status) + C(hour) + C(address)"
     lm = sm.ols(formula=myformula, data=df_merge).fit()
-    print(lm.summary())
+
+    now = time.time()
+    current = now - start
+    print("Build model:", current)
+        
+    pickle.dump(lm, open(model_name, 'wb'))
+
+    now = time.time()
+    current = now - start
+    print("Save model:", current)
+    
+    stop = time.time()
+    elapsed = stop - start
+    print("Total time elapsed:", elapsed)
+
+def loadModel(model_name):
+    start = time.time()
+    
+    
+    loaded_lm = pickle.load(open(model_name, 'rb'))
+    
+    now = time.time()
+    current = now - start
+    print("Load Model:", current)
+
+    
+    df = pd.read_csv('modelTestData.csv')
+
+    predict_df = pd.DataFrame({'PredictedBikes': loaded_lm.predict(df), 'ActualBikes': df["availableBikes"]})
+    print(predict_df[['PredictedBikes', 'ActualBikes']].head(10)) 
+
+    now = time.time()
+    current = now - start
+    print("Make Predictions:", current)
+    
+    stop = time.time()
+    elapsed = stop - start
+    print("Total time elapsed:", elapsed)
+
 
 if __name__ == '__main__':
-    main()
+    loadModel('mymodel3.sav')
