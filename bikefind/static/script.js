dd499a33260c65a1fa071551b4a41eb5df6904c8
Conor Hopkins <conor.hopkins@ucdconnect.ie>
2018-04-10 14:22:24 +0100

    javascript formatting

--- bikefind/static/script.js
+++ bikefind/static/script.js
@@ -1,136 +1,135 @@
 /**
  * Initialises map, adds onclick event to create a user marker
-**/
+ **/
 function initMap() {
 
-  var charlemontPlace = {lat: 53.330662, lng: -6.260177};
+    var charlemontPlace = {
+        lat: 53.330662,
+        lng: -6.260177
+    };
 
-  var mapOptions = {
-      zoom:15,
-      center:charlemontPlace
-  }
+    var mapOptions = {
+        zoom: 15,
+        center: charlemontPlace
+    };
 
-  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-  // adding a current position marker on user click
+    // adding a current position marker on user click
 
-  google.maps.event.addListener(map, 'click', function(event){
+    google.maps.event.addListener(map, "click", function(event) {
 
-     addCurrentPositionMarker(event.latLng)
-  return map;
-  });
+        addCurrentPositionMarker(event.latLng);
+        return map;
+    });
 
-  var currentPositionMarker;
-  /**
+    var currentPositionMarker;
+    /**
    * Creates a marker at the location clicked by the user
    * Adds an event, creates a pop-up window when the user's marker is clicked
-  **/
-  function addCurrentPositionMarker(current_position){
+   **/
+    function addCurrentPositionMarker(current_position) {
 
-    if(currentPositionMarker != null){
-      currentPositionMarker.setMap(null);
+        if (currentPositionMarker != null) {
+            currentPositionMarker.setMap(null);
+        }
+        currentPositionMarker = new google.maps.Marker({
+            title: "Selected Position",
+            position: current_position,
+            map: map,
+            Draggable: true,
+
+        });
+
+        var infowindow = new google.maps.InfoWindow({
+            content: "<div>Current position</div>"
+        });
+
+        currentPositionMarker.addListener("click", function() {
+            infowindow.open(map, currentPositionMarker);
+        });
     }
-    currentPositionMarker = new google.maps.Marker({
-      title:"Selected Position",
-      position:current_position,
-      map:map,
-      Draggable:true,
-
-    });
-
-    var infowindow = new google.maps.InfoWindow({
-      content:"<div>Current position</div>"
-    });
-
-    currentPositionMarker.addListener('click', function(){
-      infowindow.open(map, currentPositionMarker)
-    });
-  }
 
 
-     /**
-      * Makes an AJAX call to flask to retrieve static data for all stations
-      * from the db. Call function addStationMarker on each station
-     **/ 
-     function addStationMarkersFromDB(){
-          var staticData;
-          $.getJSON( "./staticTest", function( data ) {
-          $.each( data, function(key, value) {
-               if(data.hasOwnProperty(key)) {
+    /**
+   * Makes an AJAX call to flask to retrieve static data for all stations
+   * from the db. Call function addStationMarker on each station
+   **/
+    function addStationMarkersFromDB() {
+        var staticData;
+        $.getJSON("./staticTest", function(data) {
+            $.each(data, function(key, value) {
+                if (data.hasOwnProperty(key)) {
                     addStationMarker(data[key]);
-                    }
-               });
-          });  
-     }
+                }
+            });
+        });
+    }
 
-     /**
-      * Adds a new marker to the map, called for each station in staticData db table
-      * Adds two onlick events, display an info window onclick and call getLatestData
-     **/ 
-     function addStationMarker(properties){
+    /**
+   * Adds a new marker to the map, called for each station in staticData db table
+   * Adds two onlick events, display an info window onclick and call getLatestData
+   **/
+    function addStationMarker(properties) {
 
-          var marker = new google.maps.Marker({
-          title:properties.address,
-          position:new google.maps.LatLng(properties.latitude, properties.longitude),
-          map:map    
-          });
+        var marker = new google.maps.Marker({
+            title: properties.address,
+            position: new google.maps.LatLng(properties.latitude, properties.longitude),
+            map: map
+        });
 
-          var infowindow = new google.maps.InfoWindow({
-          content:properties.address
-          });
+        var infowindow = new google.maps.InfoWindow({
+            content: properties.address
+        });
 
-          marker.addListener('click', function(){
-          infowindow.open(map, marker);
-          var availableBikes = getLatestData(properties.address);
-          console.log(availableBikes);
-          });
-     }
+        marker.addListener("click", function() {
+            infowindow.open(map, marker);
+            var availableBikes = getLatestData(properties.address);
+            console.log(availableBikes);
+        });
+    }
 
-     /**
-      * Makes an AJAX request to flask, using root /rtpi. Passes the address
-      * of the requested station. Flask makes a new API call and passes the result
-      * for the requested station back as JSON. That data is then formatted and
-      * displayed in div rtpi
-     **/ 
-     function getLatestData(address){
-          $.ajax(
-          {
-          url: '/rtpi',
-          data : {
-          reqAddress : address,
-          reqJson : null
-          },
-          type: 'POST',
-          dataType: 'json',
-          })
+    /**
+   * Makes an AJAX request to flask, using root /rtpi. Passes the address
+   * of the requested station. Flask makes a new API call and passes the result
+   * for the requested station back as JSON. That data is then formatted and
+   * displayed in div rtpi
+   **/
+    function getLatestData(address) {
+        $.ajax({
+            url: "/rtpi",
+            data: {
+                reqAddress: address,
+                reqJson: null
+            },
+            type: "POST",
+            dataType: "json",
 
-          var availableBikes;
-          .done(availableBikes = function(data){
-               var timeCell = data.reqJson.last_update;
-               var addressCell = data.reqJson.address;
-               var availableBikes = data.reqJson.availableBikes;
-               var availableBikeStands = data.reqJson.availableBikeStands;
-               var status = data.reqJson.status;
-               var retString = 'Time: ' + timeCell + ' Address: ' + address + ' availableBikes: ' + availableBikes + ' availableBikeStands' + availableBikeStands;
-               $('#rtpi').text(retString)
-               //return data.reqJson.availableBikes;
-          });
-          console.log(reqAddress);
-          //return availableBikes;
-          }
+            // var availableBikes;
+        }).done(function(data) {
+            var timeCell = data.reqJson.last_update;
+            var addressCell = data.reqJson.address;
+            var availableBikes = data.reqJson.availableBikes;
+            var availableBikeStands = data.reqJson.availableBikeStands;
+            var status = data.reqJson.status;
+            var retString = "Time: " + timeCell + " Address: " + address + " availableBikes: " + availableBikes + " availableBikeStands" + availableBikeStands;
+            $("#rtpi").text(retString);
+            //return data.reqJson.availableBikes;
+        });
+        console.log(reqAddress);
+    //return availableBikes;
+    }
 
-     function getWeatherData(){
-          $('#weather').text("Success!")
-          $.getJSON( "./getWeather", function(data){
-          console.log(data)
-          var mainDes = data.mainDescription
-          var retString = "General: " + mainDes
-          $('#weather').text(retString)
-          });  
-     }
+    function getWeatherData() {
+        $("#weather").text("Success!");
+        $.getJSON("./getWeather", function(data) {
+            console.log(data);
+            var mainDes = data.mainDescription;
+            var retString = "General: " + mainDes;
+            $("#weather").text(retString);
+        });
+    }
 
-getWeatherData();
-addStationMarkersFromDB();
+    getWeatherData();
+    addStationMarkersFromDB();
 }
-
-
--- bikefind/webscraper.py
+++ bikefind/webscraper.py
@@ -1,23 +1,27 @@
 from sqlalchemy import create_engine, exc, update
 from sqlalchemy.orm.session import sessionmaker
 from bikefind.dbClasses import staticData, dynamicData, currentData, weatherData
-import requests, time, logging
+import requests
+import time
+import logging
 
-logging.basicConfig(filename='webscraper.log', level=logging.ERROR, format='%(asctime)s:%(levelname)s:%(message)s')
+logging.basicConfig(filename='webscraper.log', level=logging.ERROR,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
 
-#connect to remote DBS
+# connect to remote DBS
 db_connection_string = "mysql+cymysql://conor:team0db1@team0db.cojxdhcdsq2b.us-west-2.rds.amazonaws.com/team0"
-#db_connection_string = "mysql+cymysql://root:password@localhost/test"
+# db_connection_string = "mysql+cymysql://root:password@localhost/test"
 engine = create_engine(db_connection_string)
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
 # get from jcdecaux api and store data in list of json objects (station_info_list)
-bikes_connection_string ='https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=6e19678db44aa0bfdb4632faba1f58723758a2c4'
+bikes_connection_string = 'https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=6e19678db44aa0bfdb4632faba1f58723758a2c4'
 
 # get from openweathermap api and store data in dictionary
 weather_connection_string = 'http://api.openweathermap.org/data/2.5/weather?q=Dublin&appid=416123cec041d7c358e497cd73c9657e'
+
 
 def main():
     """Runs an infinite loop, calling DB update functions on each iteration.
@@ -25,35 +29,36 @@ def main():
     every 30 mins"""
     # add static data (once-off)
     getStaticData()
-    
+
     Session = sessionmaker(bind=engine)
     session = Session()
     getCurrentData()
-    
+
     counter = 0
     while(True):
-        #New DB session for each iteration
+        # New DB session for each iteration
         Session = sessionmaker(bind=engine)
         session = Session()
 
         getDynamicData()
 
-        #update weather every 30 minutes
+        # update weather every 30 minutes
         if counter % 6 == 0:
             getWeatherData()
 
         session.close()
         counter += 1
         print("sleeping now", counter)
-        #300 seconds - execution time for one iteration (~55s)
+        # 300 seconds - execution time for one iteration (~55s)
         time.sleep(245)
+
 
 def getStaticData():
     """Creates a table in DB for bike station static data. Populates the table
     with data from an API request, one row for each station"""
     r = requests.get(bikes_connection_string)
     station_info_list = r.json()
-    
+
     x = 0
     for station in station_info_list:
         address = station['address']
@@ -61,9 +66,9 @@ def getStaticData():
         longitude = station['position']['lng']
         banking = station['banking']
 
-        #add to db
-        static_row = staticData(address = address, latitude = latitude, 
-                                longitude = longitude, banking = banking)
+        # add to db
+        static_row = staticData(address=address, latitude=latitude,
+                                longitude=longitude, banking=banking)
         session.add(static_row)
         try:
             session.commit()
@@ -72,17 +77,18 @@ def getStaticData():
         except Exception as e:
             session.rollback()
             logging.error(e)
-        
-        #little ticker counting down the stations, because waiting sucks
+
+        # little ticker counting down the stations, because waiting sucks
         #x += 1
-        #if x % 5 == 0:
+        # if x % 5 == 0:
         #    print("Static Bikes Counted:", x, '/', len(station_info_list))
     session.close()
-    
+
+
 def getCurrentData():
     r = requests.get(bikes_connection_string)
     station_info_list = r.json()
-    
+
     x = 0
     for station in station_info_list:
         address = station['address']
@@ -92,12 +98,12 @@ def getCurrentData():
         availableBikes = station['available_bikes']
         status = station['status']
 
-        #add to db
-        current_row = currentData(address = address, last_update = last_update,
-                                totalBikeStands = totalBikeStands,
-                                availableBikeStands = availableBikeStands,
-                                availableBikes = availableBikes,
-                                status = status)
+        # add to db
+        current_row = currentData(address=address, last_update=last_update,
+                                  totalBikeStands=totalBikeStands,
+                                  availableBikeStands=availableBikeStands,
+                                  availableBikes=availableBikes,
+                                  status=status)
         session.add(current_row)
         try:
             session.commit()
@@ -106,87 +112,89 @@ def getCurrentData():
         except Exception as e:
             session.rollback()
             logging.error(e)
-        
+
         #x += 1
-        #if x % 5 == 0:
+        # if x % 5 == 0:
             #print("Current Bikes Counted:", x, '/', len(station_info_list))
     session.close()
 
+
 def getDynamicData():
 
-        r = requests.get(bikes_connection_string)
-        station_info_list = r.json()
-        for station in station_info_list:
+    r = requests.get(bikes_connection_string)
+    station_info_list = r.json()
+    for station in station_info_list:
 
-            try:
-                curr_time = station['last_update']
-            except KeyError:
-                return
-            try:
-                address = station['address']
-            except KeyError:
-                return
-            try:
-                totalBikeStands = station['bike_stands']
-            except KeyError:
-                totalBikeStands = 0
-            try:
-                availableBikeStands = station['available_bike_stands']
-            except KeyError:
-                availableBikeStands = 0
-            try:
-                availableBikes = station['available_bikes']
-            except KeyError:
-                availableBikes = 0
-            try:
-                status = station['status']
-            except KeyError:
-                status = 'default'
+        try:
+            curr_time = station['last_update']
+        except KeyError:
+            return
+        try:
+            address = station['address']
+        except KeyError:
+            return
+        try:
+            totalBikeStands = station['bike_stands']
+        except KeyError:
+            totalBikeStands = 0
+        try:
+            availableBikeStands = station['available_bike_stands']
+        except KeyError:
+            availableBikeStands = 0
+        try:
+            availableBikes = station['available_bikes']
+        except KeyError:
+            availableBikes = 0
+        try:
+            status = station['status']
+        except KeyError:
+            status = 'default'
 
-            #Create DB object with dynamicData class, then try to add it to the DB
-            dynamic_row = dynamicData(time = curr_time, address = address, 
-                                      totalBikeStands = totalBikeStands, 
-                                      availableBikeStands = availableBikeStands, 
-                                      availableBikes = availableBikes, status = status )
-            session.add(dynamic_row)
+        # Create DB object with dynamicData class, then try to add it to the DB
+        dynamic_row = dynamicData(time=curr_time, address=address,
+                                  totalBikeStands=totalBikeStands,
+                                  availableBikeStands=availableBikeStands,
+                                  availableBikes=availableBikes, status=status)
+        session.add(dynamic_row)
+        success = False
+        try:
+            session.commit()
+            success = True
+        except exc.IntegrityError:
+            session.rollback()
             success = False
-            try:
-                session.commit()
-                success = True
-            except exc.IntegrityError:
-                session.rollback()
-                success = False
-            except Exception as e:
-                session.rollback()
-                logging.error(e)
-                success = False
-                
-            if success:
-                # if the previous commit goes through, then this block checks the
-                # new data against the current values in the currentData table,
-                # and updates appropriately (should probably be a separate function)
-                
-                # find currentData row with matching address value
-                match = session.query(currentData).filter(currentData.address == address).one()
-                #print("For", match.address, "station:")
-                if curr_time > match.last_update: # check if the timestamp is different, if not, ignore
-                    #print("Before:", match.last_update, match.totalBikeStands, 
+        except Exception as e:
+            session.rollback()
+            logging.error(e)
+            success = False
+
+        if success:
+            # if the previous commit goes through, then this block checks the
+            # new data against the current values in the currentData table,
+            # and updates appropriately (should probably be a separate function)
+
+            # find currentData row with matching address value
+            match = session.query(currentData).filter(
+                currentData.address == address).one()
+            #print("For", match.address, "station:")
+            if curr_time > match.last_update:  # check if the timestamp is different, if not, ignore
+                # print("Before:", match.last_update, match.totalBikeStands,
+                #                          match.availableBikeStands, match.status)
+                # update values in row
+                match.last_update = curr_time
+                match.totalBikeStands = totalBikeStands
+                match.availableBikeStands = availableBikeStands
+                match.availableBikes = availableBikes
+                match.status = status
+                # print("After:", match.last_update, match.totalBikeStands,
 #                          match.availableBikeStands, match.status)
-                    # update values in row
-                    match.last_update = curr_time
-                    match.totalBikeStands = totalBikeStands
-                    match.availableBikeStands = availableBikeStands
-                    match.availableBikes = availableBikes
-                    match.status = status
-                    #print("After:", match.last_update, match.totalBikeStands,
-#                          match.availableBikeStands, match.status)
-                    try:
-                        session.commit()
-                    except exc.IntegrityError:
-                        session.rollback()
-                    except Exception as e:
-                        session.rollback()
-                        logging.error(e)
+                try:
+                    session.commit()
+                except exc.IntegrityError:
+                    session.rollback()
+                except Exception as e:
+                    session.rollback()
+                    logging.error(e)
 
 
 def getWeatherData():
@@ -249,15 +257,14 @@ def getWeatherData():
     except KeyError:
         w_visibility = 0
 
-
-    #Create DB object with weatherData class, then try to add it to the DB
-    weather_row = weatherData(time = w_time, mainDescription = w_mainDescription, 
-                                detailedDescription = w_detailedDescription, 
-                                icon = w_icon, currentTemp = w_temp, maxTemp = w_maxTemp, 
-                                minTemp = w_minTemp, pressure = w_pressure, 
-                                humidity = w_humidity, windSpeed = w_windSpeed, 
-                                windAngle = w_windAngle, cloudDensity = w_cloudDensity, 
-                                visibility = w_visibility)
+    # Create DB object with weatherData class, then try to add it to the DB
+    weather_row = weatherData(time=w_time, mainDescription=w_mainDescription,
+                              detailedDescription=w_detailedDescription,
+                              icon=w_icon, currentTemp=w_temp, maxTemp=w_maxTemp,
+                              minTemp=w_minTemp, pressure=w_pressure,
+                              humidity=w_humidity, windSpeed=w_windSpeed,
+                              windAngle=w_windAngle, cloudDensity=w_cloudDensity,
+                              visibility=w_visibility)
 
     session.add(weather_row)
     try:
@@ -267,6 +274,7 @@ def getWeatherData():
     except Exception as e:
         session.rollback()
         logging.error(e)
+
 
 if __name__ == '__main__':
     main()
