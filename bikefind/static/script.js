867c1b6ca2000a9a3722878e2e94f49691e7af9e
Conor Hopkins <conor.hopkins@ucdconnect.ie>
2018-04-19 10:30:31 +0100

    added margins for datetimepicker

--- bikefind/static/script.js
+++ bikefind/static/script.js
@@ -470,14 +470,6 @@ function dateTimePicker() {
         format: "dddd HH",
         // maxViewMode: 4'days'
         // todayBtn: true
-    }).focus(function() {
-        var thisCalendar = $(this);
-        $(".ui-datepicker-calendar").detach();
-        $(".ui-datepicker-close").click(function() {
-            var month = $("#ui-datepicker-div .ui-datepicker-month :selected").val();
-            var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
-            thisCalendar.datepicker("setDate", new Date(year, month, 1));
-        });
     // $("#dateTimePickerBtn").val("Get Prediction");
     });
 }
